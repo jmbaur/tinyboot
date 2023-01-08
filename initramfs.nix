bf51a92e3d81f64ff56d0d577f1dc86831b5d712
Jared Baur <jaredbaur@fastmail.com>
2023-01-08 11:43:59 -0800

    Remove unnecessary let binding

--- initramfs.nix
+++ initramfs.nix
@@ -4,15 +4,14 @@ let
     name = "initrd-env";
     paths = [ pkgsStatic.busybox tinyboot ];
   };
-  initrd = makeInitrdNG {
-    compressor = "xz";
-    contents = [
-      { object = "${initrdEnv}/bin"; symlink = "/bin"; }
-      { object = "${initrdEnv}/sbin"; symlink = "/sbin"; }
-      { object = "${initrdEnv}/linuxrc"; symlink = "/init"; }
-      { object = "${./inittab/inittab}"; symlink = "/etc/inittab"; }
-      { object = "${./inittab/rcS}"; symlink = "/etc/init.d/rcS"; }
-    ];
-  };
 in
-initrd
+makeInitrdNG {
+  compressor = "xz";
+  contents = [
+    { object = "${initrdEnv}/bin"; symlink = "/bin"; }
+    { object = "${initrdEnv}/sbin"; symlink = "/sbin"; }
+    { object = "${initrdEnv}/linuxrc"; symlink = "/init"; }
+    { object = "${./inittab/inittab}"; symlink = "/etc/inittab"; }
+    { object = "${./inittab/rcS}"; symlink = "/etc/init.d/rcS"; }
+  ];
+}
