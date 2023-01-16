98b495b7373ea78530bd4118c708145d581942fa
Jared Baur <jaredbaur@fastmail.com>
2023-01-15 20:54:21 -0800

    Collect filesystem labels and UUIDs

--- tinyboot/src/main.rs
+++ tinyboot/src/main.rs
@@ -5,6 +5,7 @@ use boot::syslinux::SyslinuxBootLoader;
 use log::LevelFilter;
 use log::{debug, error, info};
 use nix::mount;
+use std::fmt::Write;
 use std::io::{self, Read, Seek};
 use std::path::{Path, PathBuf};
 use std::str::{self, FromStr};
@@ -20,6 +21,7 @@ use tui::style::{Color, Style};
 use tui::text::Spans;
 use tui::widgets::{Block, List, ListItem, ListState, Paragraph};
 use tui::{Frame, Terminal};
+use uuid::Uuid;
 
 const NONE: Option<&'static [u8]> = None;
 
@@ -109,25 +111,80 @@ fn find_block_devices() -> anyhow::Result<Vec<PathBuf>> {
         .collect::<Vec<PathBuf>>())
 }
 
-fn detect_fs_type(p: impl AsRef<Path>) -> anyhow::Result<String> {
+// UUID, label
+#[derive(Debug, PartialEq, Eq)]
+enum FsType {
+    Ext4(String, String),
+    Fat(String, String),
+}
+
+const FAT32_MAGIC_SIGNATURE_START: u64 = 82; // 82 to 89
+const FAT32_LABEL_START: u64 = 71; // 71 to 81
+const FAT32_UUID_START: u64 = 67; // 67 to 70
+
+const EXT4_SUPERBLOCK_START: u64 = 1024;
+const EXT4_MAGIC_SIGNATURE_START: u64 = EXT4_SUPERBLOCK_START + 0x38;
+const EXT4_UUID_START: u64 = EXT4_SUPERBLOCK_START + 0x68;
+const EXT4_LABEL_START: u64 = EXT4_SUPERBLOCK_START + 0x78;
+
+fn detect_fs_type(p: impl AsRef<Path>) -> anyhow::Result<FsType> {
     let mut f = fs::File::open(p)?;
 
+    // https://www.win.tue.nl/~aeb/linux/fs/fat/fat-1.html
     {
-        f.seek(io::SeekFrom::Start(3))?;
+        f.seek(io::SeekFrom::Start(FAT32_MAGIC_SIGNATURE_START))?;
         let mut buffer = [0; 8];
         f.read_exact(&mut buffer)?;
-        if let Ok("mkfs.fat") = str::from_utf8(&buffer) {
-            return Ok(String::from("fat"));
+        if let Ok("FAT32   ") = str::from_utf8(&buffer) {
+            let uuid: String;
+            let label: String;
+            {
+                f.seek(io::SeekFrom::Start(FAT32_UUID_START))?;
+                let mut buffer = [0; 4];
+                f.read_exact(&mut buffer)?;
+                buffer.reverse();
+                let mut s = String::new();
+                write!(&mut s, "{:02X}", buffer[0]).expect("unable to write");
+                write!(&mut s, "{:02X}", buffer[1]).expect("unable to write");
+                write!(&mut s, "-").expect("unable to write");
+                write!(&mut s, "{:02X}", buffer[2]).expect("unable to write");
+                write!(&mut s, "{:02X}", buffer[3]).expect("unable to write");
+                uuid = s
+            }
+            {
+                f.seek(io::SeekFrom::Start(FAT32_LABEL_START))?;
+                let mut buffer = [0; 11];
+                f.read_exact(&mut buffer)?;
+                label = String::from_utf8(buffer.to_vec())?.trim_end().to_string();
+            }
+            return Ok(FsType::Fat(uuid, label));
         }
     }
 
+    // https://ext4.wiki.kernel.org/index.php/Ext4_Disk_Layout
     {
-        f.seek(io::SeekFrom::Start(1080))?;
+        f.seek(io::SeekFrom::Start(EXT4_MAGIC_SIGNATURE_START))?;
         let mut buffer = [0; 2];
         f.read_exact(&mut buffer)?;
         let comp_buf = &nix::sys::statfs::EXT4_SUPER_MAGIC.0.to_le_bytes()[0..2];
         if buffer == comp_buf {
-            return Ok(String::from("ext4"));
+            let uuid: Uuid;
+            {
+                f.seek(io::SeekFrom::Start(EXT4_UUID_START))?;
+                let mut buffer = [0; 16];
+                f.read_exact(&mut buffer)?;
+                uuid = Uuid::from_bytes(buffer);
+            }
+            let label: String;
+            {
+                f.seek(io::SeekFrom::Start(EXT4_LABEL_START))?;
+                let mut buffer = [0; 16];
+                f.read_exact(&mut buffer)?;
+                label = String::from_utf8(buffer.to_vec())?
+                    .trim_matches('\0')
+                    .to_string();
+            }
+            return Ok(FsType::Ext4(uuid.to_string(), label));
         }
     }
 
@@ -228,7 +285,7 @@ fn logic<B: Backend>(terminal: &mut Terminal<B>) -> anyhow::Result<()> {
             );
 
             let Ok(fstype) = detect_fs_type(dev) else { return None; };
-            debug!("detected {} fstype on {}", fstype, dev.to_str()?);
+            debug!("detected {:?} fstype on {}", fstype, dev.to_str()?);
 
             if let Err(e) = fs::create_dir_all(&mountpoint) {
                 error!("failed to create mountpoint: {e}");
@@ -238,7 +295,10 @@ fn logic<B: Backend>(terminal: &mut Terminal<B>) -> anyhow::Result<()> {
             if let Err(e) = nix::mount::mount(
                 Some(dev.as_path()),
                 &mountpoint,
-                Some(fstype.as_str()),
+                Some(match fstype {
+                    FsType::Ext4(..) => "ext4",
+                    FsType::Fat(..) => "fat",
+                }),
                 mount::MsFlags::MS_RDONLY,
                 NONE,
             ) {
@@ -429,4 +489,22 @@ fn main() -> anyhow::Result<()> {
     terminal.show_cursor()?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    #[ignore]
+    // TODO(jared): figure out how to run these commands before running tests and cleanup after.
+    // TODO(jared): Run these commands for setup:
+    // dd bs=512M count=1 if=/dev/zero of=/tmp/disk.fat
+    // mkfs.fat -n FOOBAR /tmp/disk.fat
+    // dd bs=512M count=1 if=/dev/zero of=/tmp/disk.ext4
+    // mkfs.ext4 -L foobar /tmp/disk.ext4
+    fn detect_fs_type() {
+        let fstype = super::detect_fs_type("/tmp/disk.fat").unwrap();
+        eprintln!("{:#?}", fstype);
+        let fstype = super::detect_fs_type("/tmp/disk.ext4").unwrap();
+        eprintln!("{:#?}", fstype);
+    }
 }
