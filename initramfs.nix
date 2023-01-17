8928a3e03a6fa114ec22fa167199efa5b426b497
Jared Baur <jaredbaur@fastmail.com>
2023-01-16 16:18:40 -0800

    Remove etc directory

--- initramfs.nix
+++ initramfs.nix
@@ -1,9 +1,33 @@
-{ lib, makeInitrdNG, pkgsStatic, buildEnv, tinyboot, substituteAll, tinybootTTY ? "tty1", shellTTY ? "tty2", ... }:
+{ tinybootLog ? "debug"
+, tinybootTTY ? "tty1"
+, shellTTY ? "tty2"
+, makeInitrdNG
+, pkgsStatic
+, buildEnv
+, tinyboot
+, writeScript
+, writeText
+, ...
+}:
 let
-  initrdEnv = buildEnv {
-    name = "initrd-env";
-    paths = [ pkgsStatic.busybox tinyboot ];
-  };
+  initrdEnv = buildEnv { name = "initrd-env"; paths = [ pkgsStatic.busybox tinyboot ]; };
+  rcS = writeScript "rcS" ''
+    #!/bin/sh
+    mkdir -p /dev/pts /sys /proc /tmp /mnt
+    mount -t proc proc /proc
+    mount -t sysfs sysfs /sys
+    mount -t tmpfs tmpfs /tmp
+    mount -t devpts devpts /dev/pts
+    mdev -s
+  '';
+  inittab = writeText "inittab" ''
+    ::sysinit:/etc/init.d/rcS
+    ::ctrlaltdel:/sbin/reboot
+    ::shutdown:/bin/umount -a -r
+    ::restart:/init
+    ${tinybootTTY}::respawn:/bin/tinyboot tinyboot.log=${tinybootLog}
+    ${shellTTY}::askfirst:/bin/sh
+  '';
 in
 makeInitrdNG {
   compressor = "xz";
@@ -11,10 +35,7 @@ makeInitrdNG {
     { object = "${initrdEnv}/bin"; symlink = "/bin"; }
     { object = "${initrdEnv}/sbin"; symlink = "/sbin"; }
     { object = "${initrdEnv}/linuxrc"; symlink = "/init"; }
-    { object = ./etc/init.d/rcS; symlink = "/etc/init.d/rcS"; }
-    {
-      object = substituteAll { src = ./etc/inittab; inherit tinybootTTY shellTTY; };
-      symlink = "/etc/inittab";
-    }
+    { object = "${rcS}"; symlink = "/etc/init.d/rcS"; }
+    { object = "${inittab}"; symlink = "/etc/inittab"; }
   ];
 }
