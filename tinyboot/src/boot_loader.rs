e36cb580dbdb62b98787568336e0bb052869049b
Jared Baur <jaredbaur@fastmail.com>
2023-02-20 11:19:30 -0800

    Use `syscalls` crate instead of calling inline asm

--- tinyboot/src/boot_loader.rs
+++ tinyboot/src/boot_loader.rs
@@ -4,7 +4,8 @@ use std::fmt::{self, Display};
 use std::os::fd::AsRawFd;
 use std::path::Path;
 use std::time::{self, Duration};
-use std::{arch, error, ffi, fs, io, thread};
+use std::{error, ffi, fs, io, thread};
+use syscalls::{syscall, Sysno};
 
 #[derive(Debug)]
 pub enum Error {
@@ -62,40 +63,16 @@ pub fn kexec_load(kernel: &Path, initrd: &Path, cmdline: &str) -> io::Result<()>
     let cmdline = ffi::CString::new(cmdline)?;
     let cmdline = cmdline.to_bytes_with_nul();
 
-    let retval: usize;
-
-    #[cfg(target_arch = "aarch64")]
-    unsafe {
-        // NOTE: this is not defined in rust's libc crate for musl aarch64, see
-        // https://git.musl-libc.org/cgit/musl/tree/arch/aarch64/bits/syscall.h.in#n279 for the definition
-        // of aarch64's kexec_file_load.
-        const SYS_KEXEC_FILE_LOAD: std::ffi::c_long = 294;
-
-        arch::asm!(
-            "svc #0",
-            in("w8") SYS_KEXEC_FILE_LOAD,
-            inout("x0") kernel => retval,
-            in("x1") initrd,
-            in("x2") cmdline.len(),
-            in("x3") cmdline.as_ptr(),
-            in("x4") 0,
-            in("x5") 0,
-        );
-    }
-
-    #[cfg(target_arch = "x86_64")]
-    unsafe {
-        arch::asm!(
-            "syscall",
-            inout("rax") libc::SYS_kexec_file_load => retval,
-            in("rdi") kernel,
-            in("rsi") initrd,
-            in("rdx") cmdline.len(),
-            in("r10") cmdline.as_ptr(),
-            in("r8") 0,
-            in("r9") 0,
-        );
-    }
+    let retval = unsafe {
+        syscall!(
+            Sysno::kexec_file_load,
+            kernel,
+            initrd,
+            cmdline.len(),
+            cmdline.as_ptr(),
+            0
+        )?
+    };
 
     if retval > -4096isize as usize {
         let code = -(retval as isize) as i32;
