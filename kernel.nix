e718eed6bcbfe81e5b168694362410b4b7010b32
Jared Baur <jaredbaur@fastmail.com>
2023-03-23 23:55:24 -0700

    Use better builtin kernel build-support within nix

--- kernel.nix
+++ kernel.nix
@@ -1,8 +1,7 @@
-{ lib, stdenv, linuxKernel, linuxManualConfig, ... }:
+{ lib, stdenv, linuxKernel, ... }:
 linuxKernel.manualConfig {
   inherit lib stdenv;
   inherit (linuxKernel.kernels.linux_6_1) src version modDirVersion kernelPatches extraMakeFlags;
   configfile = ./configs/${stdenv.hostPlatform.linuxArch}-linux.config;
-  # DTB required to be set to true for nixpkgs kernel build to install dtbs into derivation
-  config.DTB = !stdenv.hostPlatform.isx86_64;
+  config = stdenv.hostPlatform.linux-kernel;
 }
