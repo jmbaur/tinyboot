07a7512dc0ad567938f8871ea396d6e7846c5b1b
Jared Baur <jaredbaur@fastmail.com>
2023-04-22 10:16:26 -0700

    Add more SPI support for x86

--- tinyboot/src/tpm.c
+++ tinyboot/src/tpm.c
@@ -1,30 +1,26 @@
-#include <stdio.h>
 #include <wolftpm/tpm2_wrap.h>
 
 const char *pcr_extend(const BYTE digest[TPM_SHA256_DIGEST_SIZE]) {
-  union {
-    PCR_Read_In pcrRead;
-    PCR_Extend_In pcrExtend;
-    byte maxInput[MAX_COMMAND_SIZE];
-  } cmdIn;
-
   int rc = -1;
+
+  PCR_Extend_In pcrExtend;
   WOLFTPM2_DEV dev;
+
   rc = wolfTPM2_Init(&dev, NULL, NULL);
   if (rc != TPM_RC_SUCCESS) {
     goto exit;
   }
 
-  XMEMSET(&cmdIn.pcrExtend, 0, sizeof(cmdIn.pcrExtend));
-  cmdIn.pcrExtend.pcrHandle = 9;
-  cmdIn.pcrExtend.digests.count = 1;
-  cmdIn.pcrExtend.digests.digests[0].hashAlg = TPM_ALG_SHA256;
+  XMEMSET(&pcrExtend, 0, sizeof(pcrExtend));
+  pcrExtend.pcrHandle = 9;
+  pcrExtend.digests.count = 1;
+  pcrExtend.digests.digests[0].hashAlg = TPM_ALG_SHA256;
 
   for (int i = 0; i < TPM_SHA256_DIGEST_SIZE; i++) {
-    cmdIn.pcrExtend.digests.digests[0].digest.H[i] = digest[i];
+    pcrExtend.digests.digests[0].digest.H[i] = digest[i];
   }
 
-  rc = TPM2_PCR_Extend(&cmdIn.pcrExtend);
+  rc = TPM2_PCR_Extend(&pcrExtend);
   if (rc != TPM_RC_SUCCESS) {
     goto exit;
   }
