335eb2e820afe2f19d2ae2f1c1f87f1b259f5906
Jared Baur <jaredbaur@fastmail.com>
2023-05-16 07:01:28 -0700

    Add shell completions

--- tinyboot/default.nix
+++ tinyboot/default.nix
@@ -1,11 +1,15 @@
-{ buildFeatures ? [ ], verifiedBootPublicKey ? "/dev/null", rustPlatform, wolftpm, pkg-config }:
+{ buildFeatures ? [ ], verifiedBootPublicKey ? "/dev/null", rustPlatform, wolftpm, pkg-config, installShellFiles }:
 rustPlatform.buildRustPackage {
   pname = "tinyboot";
   version = "0.1.0";
   src = ./.;
-  nativeBuildInputs = [ rustPlatform.bindgenHook pkg-config ];
+  nativeBuildInputs = [ rustPlatform.bindgenHook pkg-config installShellFiles ];
   buildInputs = [ wolftpm ];
   cargoLock.lockFile = ./Cargo.lock;
   VERIFIED_BOOT_PUBLIC_KEY = verifiedBootPublicKey;
   inherit buildFeatures;
+  postInstall = ''
+    installShellCompletion \
+      ./target/*/$cargoBuildType/build/tbootctl-*/out/{tbootctl.bash,tbootctl.fish,_tbootctl}
+  '';
 }
