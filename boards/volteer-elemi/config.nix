fe1865ef5264a2c003ea2dd0e0f2cc07142c5eb6
Jared Baur <jaredbaur@fastmail.com>
2023-05-18 08:13:21 -0700

    Fix volteer-elemi default config

--- boards/volteer-elemi/config.nix
+++ boards/volteer-elemi/config.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, ... }: {
   platforms = [ "x86_64-linux" ];
   kernel = {
-    configFile = lib.mkDefault pkgs.concatText "volteer-elemi-kernel.config" [ ../generic-kernel.config ../x86_64-kernel.config ../chromebook-kernel.config ];
+    configFile = lib.mkDefault (pkgs.concatText "volteer-elemi-kernel.config" [ ../generic-kernel.config ../x86_64-kernel.config ../chromebook-kernel.config ]);
     commandLine = lib.mkDefault [ "console=ttyS0" "console=tty0" ];
   };
   tinyboot.measuredBoot.enable = lib.mkDefault true;
