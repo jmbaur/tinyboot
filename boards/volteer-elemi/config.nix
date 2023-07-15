6edd0a295da5f710b38348a26181e23122b51e5d
Jared Baur <jaredbaur@fastmail.com>
2023-07-14 17:41:02 -0700

    Tweak bootsplash on volteer-elemi

--- boards/volteer-elemi/config.nix
+++ boards/volteer-elemi/config.nix
@@ -5,7 +5,10 @@
       configFile = lib.mkDefault (pkgs.concatText "volteer-elemi-kernel.config" [ ../generic-kernel.config ../x86_64-kernel.config ../chromebook-kernel.config ]);
       commandLine = [ "quiet" ];
     };
-    coreboot = { configFile = lib.mkDefault ./coreboot.config; bootsplash.enable = true; };
+    coreboot = {
+      configFile = lib.mkDefault ./coreboot.config;
+      bootsplash = { enable = true; width = 1920; height = 1080; };
+    };
     flashrom.extraArgs = lib.mkDefault [ "--ifd" "-i" "bios" ];
   };
 }
