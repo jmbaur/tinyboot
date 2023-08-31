22db35a61d25b6e30878fe28d720494e3f643741
Jared Baur <jaredbaur@fastmail.com>
2023-08-30 21:09:05 -0700

    Use deterministic version string that isn't meaningless

--- coreboot.nix
+++ coreboot.nix
@@ -2,11 +2,11 @@
 lib.makeOverridable ({ board ? null, configFile, extraConfig ? "", extraArgs ? { } }:
 let
   toolchain = pkgsBuildBuild.coreboot-toolchain.${{ i386 = "i386"; x86_64 = "i386"; arm64 = "aarch64"; arm = "arm"; riscv = "riscv"; powerpc = "ppc64"; }.${stdenvNoCC.hostPlatform.linuxArch}};
-in
-stdenvNoCC.mkDerivation ({
-  pname = "coreboot-${if (board) != null then board else "unknown"}";
   version = src.shortRev or src.dirtyShortRev; # allow for --override-input
-  src = "${src}";
+in
+stdenvNoCC.mkDerivation (rec {
+  pname = "coreboot-${if (board) != null then board else "unknown"}";
+  inherit src version;
   patches = [ ./patches/coreboot-fitimage-memlayout.patch ];
   depsBuildBuild = [ pkgsBuildBuild.stdenv.cc pkg-config openssl ];
   nativeBuildInputs = [ python3 ];
@@ -23,7 +23,7 @@ stdenvNoCC.mkDerivation ({
     make oldconfig
     runHook postConfigure
   '';
-  makeFlags = [ "XGCCPATH=${toolchain}/bin/" "BUILD_TIMELESS=1" "UPDATED_SUBMODULES=1" ];
+  makeFlags = [ "XGCCPATH=${toolchain}/bin/" "KERNELVERSION=${version}" "UPDATED_SUBMODULES=1" ];
   installPhase = ''
     runHook preInstall
     mkdir -p  $out
