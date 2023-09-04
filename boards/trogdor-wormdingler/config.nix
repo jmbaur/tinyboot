f34ab16ce7ca5be95a8bd5151993bf8662ef99fa
Jared Baur <jaredbaur@fastmail.com>
2023-09-04 16:21:20 -0700

    Remove venus firmware from trogdor
    It is too large and doesn't fit within the ROM chip.

--- boards/trogdor-wormdingler/config.nix
+++ boards/trogdor-wormdingler/config.nix
@@ -7,9 +7,6 @@
       # https://gitlab.freedesktop.org/drm/msm/-/issues/13
       commandLine = [ "pd_ignore_unused" "clk_ignore_unused" "quiet" ];
       dtbPattern = "sc7180-trogdor-wormdingler*";
-      firmware = pkgs.runCommand "wormdingler-firmware" { } ''
-        mkdir -p $out/qcom; cp -r ${pkgs.linux-firmware}/lib/firmware/qcom/venus-5.4 $out/qcom/venus-5.4
-      '';
     };
     coreboot.configFile = lib.mkDefault ./coreboot.config;
     tinyboot.ttys = [ "ttyMSM0" ];
