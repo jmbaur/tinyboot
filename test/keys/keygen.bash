6caee731da537a3a943fca78bd5022cdbd88c9b4
Jared Baur <jaredbaur@fastmail.com>
2023-10-23 07:46:57 -0700

    Write openssl config separate from script

--- test/keys/keygen.bash
+++ test/keys/keygen.bash
@@ -1,32 +1,8 @@
-#!/usr/bin/env bash
-
-out="${1:-$(dirname "$0")}"
-
-GENKEY="${out}/x509_ima.genkey"
-
-cat <<EOF >"$GENKEY"
-[ req ]
-default_bits = 4096
-distinguished_name = req_distinguished_name
-prompt = no
-string_mask = utf8only
-x509_extensions = myexts
-
-[ req_distinguished_name ]
-O = tinyhost
-CN = tinyboot test signing key
-emailAddress = tboot@tinyhost
-
-[ myexts ]
-basicConstraints=critical,CA:FALSE
-keyUsage=digitalSignature
-subjectKeyIdentifier=hash
-authorityKeyIdentifier=keyid
-EOF
-
-openssl req -x509 -new -nodes -utf8 -sha1 -days 3650 -batch -config "$GENKEY" \
-	-outform DER -out "${out}/x509_ima.der" -keyout "${out}/privkey_ima.pem"
-
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p openssl
+# shellcheck shell=bash
+script_dir="$(dirname "$0")"
+out="${1:-$script_dir}"
+openssl req -x509 -new -nodes -utf8 -sha1 -days 3650 -batch -config "${script_dir}/openssl.cnf" -outform DER -out "${out}/x509_ima.der" -keyout "${out}/privkey_ima.pem"
 openssl x509 -inform DER -in "${out}/x509_ima.der" -out "${out}/x509_ima.pem"
-
 openssl rsa -pubout -in "${out}/privkey_ima.pem" -out "${out}/pubkey_ima.pem"
