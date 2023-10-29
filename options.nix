893988546635768ec009e8a5830175bb675c1a6a
Jared Baur <jaredbaur@fastmail.com>
2023-10-29 05:55:14 +0000

    Add options for VPD contents

--- options.nix
+++ options.nix
@@ -19,6 +19,18 @@ let
     compressor = "xz";
     contents = [{ object = "${pkgs.busybox}/bin/busybox"; symlink = "/init"; } { object = "${pkgs.busybox}/bin"; symlink = "/bin"; }];
   };
+  vpdOption = lib.mkOption {
+    type = lib.types.attrsOf (lib.types.either lib.types.str lib.types.path);
+    default = { };
+  };
+  applyVpd = vpdPartition: key: value:
+    if lib.isPath value then ''
+      base64 <${value} >tmp.base64
+      vpd -f $out -i ${vpdPartition} -S ${key}=tmp.base64
+      rm tmp.base64
+    '' else ''
+      vpd -f $out -i ${vpdPartition} -s ${key}=${value}
+    '';
 in
 {
   imports = lib.mapAttrsToList (board: _: ./boards/${board}/config.nix) boards;
@@ -49,6 +61,8 @@ in
     coreboot = {
       enable = mkEnableOption "coreboot integration";
       configFile = mkOption { type = types.path; };
+      vpd.ro = vpdOption;
+      vpd.rw = vpdOption;
       extraConfig = mkOption { type = types.lines; default = ""; };
     };
     verifiedBoot = {
@@ -75,6 +89,7 @@ in
     # The "--" makes linux pass remaining parameters as args to PID1
     linux.commandLine = [ "console=ttynull" "--" "tboot.loglevel=${config.loglevel}" "tboot.tty=${config.tinyboot.tty}" "tboot.programmer=${config.flashrom.programmer}" ];
 
+    coreboot.vpd.ro.pubkey = config.verifiedBoot.signingPublicKey;
     coreboot.extraConfig = ''
       CONFIG_VBOOT_ROOT_KEY="${config.verifiedBoot.vbootRootKey}"
       CONFIG_VBOOT_RECOVERY_KEY="${config.verifiedBoot.vbootRecoveryKey}"
@@ -121,37 +136,41 @@ in
       firmware = pkgs.runCommand "tinyboot-${config.build.coreboot.name}"
         {
           inherit (config.verifiedBoot) requiredSystemFeatures;
-          nativeBuildInputs = with pkgs.buildPackages; [ cbfstool vboot_reference ];
+          nativeBuildInputs = with pkgs.buildPackages; [ cbfstool vboot_reference vpd ];
           passthru = { inherit (config.build) linux initrd coreboot; };
           meta.platforms = config.platforms;
           env.CBFSTOOL = "${pkgs.buildPackages.cbfstool}/bin/cbfstool"; # needed by futility
         }
         ''
-          dd if=${config.build.coreboot}/coreboot.rom of=$out
+            dd if=${config.build.coreboot}/coreboot.rom of=$out
 
-          cbfstool $out expand -r FW_MAIN_A
-          ${if pkgs.stdenv.hostPlatform.linuxArch == "x86_64" then ''
-          cbfstool $out add-payload \
-            -r FW_MAIN_A \
-            -n fallback/payload \
-            -f ${config.build.linux}/${pkgs.stdenv.hostPlatform.linux-kernel.target} \
-            -I ${config.build.initrd}/initrd
-          '' else if pkgs.stdenv.hostPlatform.linuxArch == "arm64" then ''
-          cbfstool $out add \
-            -r FW_MAIN_A \
-            -n fallback/payload \
-            -t fit_payload \
-            -f ${config.build.fitImage}/uImage
+            cbfstool $out expand -r FW_MAIN_A
+            ${if pkgs.stdenv.hostPlatform.linuxArch == "x86_64" then ''
+            cbfstool $out add-payload \
+              -r FW_MAIN_A \
+              -n fallback/payload \
+              -f ${config.build.linux}/${pkgs.stdenv.hostPlatform.linux-kernel.target} \
+              -I ${config.build.initrd}/initrd
+            '' else if pkgs.stdenv.hostPlatform.linuxArch == "arm64" then ''
+            cbfstool $out add \
+              -r FW_MAIN_A \
+              -n fallback/payload \
+              -t fit_payload \
+              -f ${config.build.fitImage}/uImage
           '' else throw "Unsupported architecture"}
-          cbfstool $out truncate -r FW_MAIN_A
+            cbfstool $out truncate -r FW_MAIN_A
 
-          futility sign \
-            --signprivate "${config.verifiedBoot.vbootFirmwarePrivkey}" \
-            --keyblock "${config.verifiedBoot.vbootKeyblock}" \
-            --kernelkey "${config.verifiedBoot.vbootKernelKey}" \
-            --version ${config.verifiedBoot.vbootKeyblockVersion} \
-            --flags ${config.verifiedBoot.vbootKeyblockPreambleFlags} \
-            $out
+            vpd -f $out -O
+            ${lib.concatLines (lib.mapAttrsToList (applyVpd "RO_VPD") config.coreboot.vpd.ro)}
+            ${lib.concatLines (lib.mapAttrsToList (applyVpd "RW_VPD") config.coreboot.vpd.rw)}
+
+            futility sign \
+              --signprivate "${config.verifiedBoot.vbootFirmwarePrivkey}" \
+              --keyblock "${config.verifiedBoot.vbootKeyblock}" \
+              --kernelkey "${config.verifiedBoot.vbootKernelKey}" \
+              --version ${config.verifiedBoot.vbootKeyblockVersion} \
+              --flags ${config.verifiedBoot.vbootKeyblockPreambleFlags} \
+              $out
         '';
       installScript = pkgs.writeShellScriptBin "install-tinyboot" ''
         kexec -l ${config.build.linux}/${pkgs.stdenv.hostPlatform.linux-kernel.target} --initrd=${installInitrd}/initrd
