d2423ce26f088c5346838972deb2d1b1b5cf05d1
Jared Baur <jaredbaur@fastmail.com>
2023-11-23 00:05:13 -0800

    Update inputs

--- pkgs/flashrom-cros/default.nix
+++ pkgs/flashrom-cros/default.nix
@@ -1,18 +1,17 @@
-{ useMeson ? true, lib, stdenv, fetchgit, cmocka, meson, ninja, libftdi1, libjaylink, libusb1, pciutils, pkg-config, sphinx, bash-completion, ... }:
+{ stdenv, fetchgit, cmocka, meson, ninja, libftdi1, libjaylink, libusb1, pciutils, pkg-config, sphinx, bash-completion, ... }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "flashrom-cros";
   version = "unstable-${builtins.substring 0 7 finalAttrs.src.rev}";
   src = fetchgit {
     url = "https://chromium.googlesource.com/chromiumos/third_party/flashrom";
     branchName = "master";
-    rev = "28a0ab09617e39f85ddac1100acfaca205843182";
-    hash = "sha256-QK0/e2yr2Z/MDnrZJ1P+Rto9zdepvKp449v4AF2ulXA=";
+    rev = "a96ce7b9f3083b2cc63e8a86674e7565e718f20e";
+    hash = "sha256-Z1i95pzTcQhbByLSKWHQdAwLGAEyFgr4UNJWhwin24U=";
   };
   patches = [ ./power-management.patch ];
-  outputs = [ "out" ] ++ lib.optionals useMeson [ "lib" "dev" ];
-  dontUseCmakeConfigure = useMeson;
-  nativeBuildInputs = lib.optionals useMeson [ meson ninja ] ++ [ pkg-config sphinx bash-completion ];
+  outputs = [ "out" "lib" "dev" ];
+  dontUseCmakeConfigure = true;
+  nativeBuildInputs = [ meson ninja pkg-config sphinx bash-completion ];
   buildInputs = [ cmocka libftdi1 libusb1 pciutils libjaylink ];
-  installFlags = lib.optional (!useMeson) "PREFIX=$(out)";
   meta.mainProgram = "flashrom";
 })
