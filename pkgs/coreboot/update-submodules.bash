c32354b63b2265962f29a365ee64c5643cda1c83
Jared Baur <jaredbaur@fastmail.com>
2024-05-09 15:40:31 -0700

    Fix coreboot FODs

--- pkgs/coreboot/update-submodules.bash
+++ pkgs/coreboot/update-submodules.bash
@@ -5,9 +5,9 @@
 
 cd "$(dirname "$0")" || exit
 
-nix-prefetch-git https://review.coreboot.org/amd_blobs --rev e4519efca74615f0f322d595afa7702c64753914 >amd_blobs.json
-nix-prefetch-git https://review.coreboot.org/blobs.git --rev a8db7dfe823def043368857b8fbfbba86f2e9e47 >blobs.json
-nix-prefetch-git https://review.coreboot.org/cmocka.git --rev 8931845c35e78b5123d73430b071affd537d5935 >cmocka.json
-nix-prefetch-git https://review.coreboot.org/fsp.git --rev 481ea7cf0bae0107c3e14aa746e52657647142f3 >fsp.json
-nix-prefetch-git https://review.coreboot.org/intel-microcode.git --rev 6788bb07eb5f9e9b83c31ea1364150fe898f450a >intel_microcode.json
-nix-prefetch-git https://review.coreboot.org/qc_blobs.git --rev a252198ec6544e13904cfe831cec3e784aaa715d >qc_blobs.json
+nix-prefetch-git https://review.coreboot.org/amd_blobs --rev 64cdd7c8ef199f5d79be14e7972fb7316f41beed >amd_blobs.json
+nix-prefetch-git https://review.coreboot.org/blobs --rev a8db7dfe823def043368857b8fbfbba86f2e9e47 >blobs.json
+nix-prefetch-git https://review.coreboot.org/cmocka --rev 8931845c35e78b5123d73430b071affd537d5935 >cmocka.json
+nix-prefetch-git https://review.coreboot.org/fsp --rev 507ef01cce16dc1e1af898e60de96dbb8e9d6d17 >fsp.json
+nix-prefetch-git https://review.coreboot.org/intel-microcode --rev ece0d294a29a1375397941a4e6f2f7217910bc89 >intel_microcode.json
+nix-prefetch-git https://review.coreboot.org/qc_blobs --rev a252198ec6544e13904cfe831cec3e784aaa715d >qc_blobs.json
