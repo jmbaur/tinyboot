273d15fd36ba49850a5cff6c63d5c7499fbb6898
Jared Baur <jaredbaur@fastmail.com>
2024-06-22 10:55:10 -0700

    Fix build

--- src/test.zig
+++ src/test.zig
@@ -1,12 +1,18 @@
-// all top-level files, populates @This() for below
+// all non-entrypoint files, populates @This() for below
 pub const bls = @import("./boot/bls.zig");
+pub const boot = @import("./boot.zig");
 pub const bootspec = @import("./bootspec.zig");
+pub const client = @import("./client.zig");
 pub const device = @import("./device.zig");
 pub const filesystem = @import("./disk/filesystem.zig");
+pub const log = @import("./log.zig");
+pub const message = @import("./message.zig");
 pub const partition_table = @import("./disk/partition_table.zig");
-pub const tboot_bless_boot = @import("./tboot-bless-boot.zig");
-pub const tboot_bless_boot_generator = @import("./tboot-bless-boot-generator.zig");
-pub const tboot_loader = @import("./tboot-loader.zig");
+pub const security = @import("./security.zig");
+pub const server = @import("./server.zig");
+pub const system = @import("./system.zig");
+pub const tmp = @import("./tmp.zig");
+pub const xmodem = @import("./boot/xmodem.zig");
 
 test {
     // recursively test all imported files
