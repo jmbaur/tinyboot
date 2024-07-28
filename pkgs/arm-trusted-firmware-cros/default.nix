ecf5293eead78744765b7dbe4aed3854e446d837
Jared Baur <jaredbaur@fastmail.com>
2024-07-28 12:55:50 -0700

    Update ATF

--- pkgs/arm-trusted-firmware-cros/default.nix
+++ pkgs/arm-trusted-firmware-cros/default.nix
@@ -8,13 +8,14 @@ buildArmTrustedFirmware {
   version = "2.10.0";
   src = fetchgit {
     url = "https://chromium.googlesource.com/chromiumos/third_party/arm-trusted-firmware";
-    branchName = "release-R125-15853.B";
-    rev = "17bef2248d4547242463e27cfe48ec96029626b4";
-    hash = "sha256-4Y50B+Xa8fRTa/0R8nm5KQhv1gU9MtAxyh6l5RO5LBM=";
+    branchName = "release-R128-15964.B";
+    rev = "c0d660ac2911453c21d4868e46af714f508f2c19";
+    hash = "sha256-7Ew6zm1pAb608sVBKWGeTnNZ2z07nE/xWcwQrnvW5yU=";
   };
   filesToInstall = [ "build/${platform}/release/bl31/bl31.elf" ];
-  extraMakeFlags = [ "COREBOOT=1" ];
+  extraMakeFlags = [ "DISABLE_BIN_GENERATION=1" "COREBOOT=1" ];
   dontStrip = false;
+  patches = [ ./toolchain.patch ];
   postInstall = ''
     mkdir -p $out/libexec && mv $out/bl31.elf $out/libexec/bl31.elf
   '';
