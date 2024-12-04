add622608bd93981de573cd7a9974b22f129ef79
Jared Baur <jaredbaur@fastmail.com>
2024-12-04 10:22:16 -0800

    Add symlink support to cpio archives

--- src/cpio/main.zig
+++ src/cpio/main.zig
@@ -24,41 +24,35 @@ const CpioHeader = struct {
     check: u32 = 0,
 };
 
-const CpioEntry = struct {
-    header: CpioHeader,
-    filename: []const u8,
-    source: ?*std.io.StreamSource,
-};
-
 const CpioEntryType = enum {
     Directory,
     File,
+    Symlink,
 
-    fn toMode(self: @This()) u32 {
+    fn toMode(self: @This(), perms: u32) u32 {
         return @as(u32, switch (self) {
-            .Directory => std.posix.S.IFDIR,
-            .File => std.posix.S.IFREG,
+            .Directory => std.posix.S.IFDIR | perms,
+            .File => std.posix.S.IFREG | perms,
+            .Symlink => std.posix.S.IFLNK | 0o777, // symlinks are always 0o777
         });
     }
 };
 
 pub const CpioArchive = struct {
+    dest: *std.io.StreamSource,
     ino: u32 = 0,
-    entries: std.ArrayList(CpioEntry),
+    total_written: usize = 0,
 
     const Error = error{
         FileTooLarge,
+        UnexpectedSource,
     };
 
-    pub fn init(allocator: std.mem.Allocator) !@This() {
-        var cpio = @This(){
-            .entries = std.ArrayList(CpioEntry).init(allocator),
-        };
-
-        errdefer cpio.entries.deinit();
+    pub fn init(dest: *std.io.StreamSource) !@This() {
+        var cpio = @This(){ .dest = dest };
 
         // Always start off with a root directory.
-        try cpio.addEntry(null, "./", .Directory, 0o755);
+        try cpio.addEntry(null, ".", .Directory, 0o755);
 
         return cpio;
     }
@@ -70,6 +64,10 @@ pub const CpioArchive = struct {
         entry_type: CpioEntryType,
         perms: u32,
     ) !void {
+        if (entry_type == .Directory and source != null) {
+            return Error.UnexpectedSource;
+        }
+
         const filesize = b: {
             if (source) |s| {
                 const pos = try s.getEndPos();
@@ -83,116 +81,126 @@ pub const CpioArchive = struct {
             }
         };
 
-        const mode = entry_type.toMode() | perms;
-
         // add null terminator
         const filepath = path ++ [_]u8{0};
 
-        try self.entries.append(.{
-            .header = .{
+        // write entry to archive
+        {
+            const header = CpioHeader{
                 .ino = self.ino,
-                .mode = mode,
+                .mode = entry_type.toMode(perms),
                 .uid = 0,
                 .gid = 0,
-                .nlink = if (entry_type == .File) 1 else 2,
+                .nlink = if (entry_type == .Directory) 2 else 1,
                 .filesize = filesize,
                 .devmajor = 0,
                 .devminor = 0,
                 .rdevmajor = 0,
                 .rdevminor = 0,
                 .namesize = @intCast(filepath.len),
-            },
-            .source = source,
-            .filename = filepath,
-        });
+            };
 
-        self.ino += 1;
-    }
-
-    pub fn finalize(self: *@This(), dest: *std.io.StreamSource) !void {
-        try self.addEntry(null, TRAILER, .File, 0);
-
-        var total_written: usize = 0;
-
-        for (self.entries.items) |entry| {
-            try dest.writer().print("{X:0>6}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}", .{
-                entry.header.magic,
-                entry.header.ino,
-                entry.header.mode,
-                entry.header.uid,
-                entry.header.gid,
-                entry.header.nlink,
-                entry.header.mtime,
-                entry.header.filesize,
-                entry.header.devmajor,
-                entry.header.devminor,
-                entry.header.rdevmajor,
-                entry.header.rdevminor,
-                entry.header.namesize,
-                entry.header.check,
+            try self.dest.writer().print("{X:0>6}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}{X:0>8}", .{
+                header.magic,
+                header.ino,
+                header.mode,
+                header.uid,
+                header.gid,
+                header.nlink,
+                header.mtime,
+                header.filesize,
+                header.devmajor,
+                header.devminor,
+                header.rdevmajor,
+                header.rdevminor,
+                header.namesize,
+                header.check,
             });
-            total_written += ASCII_CPIO_HEADER_SIZE;
+            self.total_written += ASCII_CPIO_HEADER_SIZE;
 
-            try dest.writer().writeAll(entry.filename);
-            total_written += entry.filename.len;
+            try self.dest.writer().writeAll(filepath);
+            self.total_written += filepath.len;
 
             // pad the file name
-            const header_padding = (4 - ((ASCII_CPIO_HEADER_SIZE + entry.filename.len) % 4)) % 4;
-            try dest.writer().writeByteNTimes(0, header_padding);
-            total_written += header_padding;
+            const header_padding = (4 - ((ASCII_CPIO_HEADER_SIZE + filepath.len) % 4)) % 4;
+            try self.dest.writer().writeByteNTimes(0, header_padding);
+            self.total_written += header_padding;
 
-            if (entry.source) |source| {
+            if (source) |_source| {
                 var pos: usize = 0;
-                const end = try source.getEndPos();
+                const end = try _source.getEndPos();
 
                 var buf = [_]u8{0} ** 4096;
 
                 while (pos < end) {
-                    try source.seekTo(pos);
-                    const bytes_read = try source.read(&buf);
-                    try dest.writer().writeAll(buf[0..bytes_read]);
-                    total_written += bytes_read;
+                    try _source.seekTo(pos);
+                    const bytes_read = try _source.read(&buf);
+                    try self.dest.writer().writeAll(buf[0..bytes_read]);
+                    self.total_written += bytes_read;
                     pos += bytes_read;
                 }
 
                 // pad the file data
                 const filedata_padding = (4 - (end % 4)) % 4;
-                try dest.writer().writeByteNTimes(0, filedata_padding);
-                total_written += filedata_padding;
+                try self.dest.writer().writeByteNTimes(0, filedata_padding);
+                self.total_written += filedata_padding;
             }
         }
 
-        // Maintain a block size of 512 by adding padding to the end of the
-        // archive.
-        try dest.writer().writeByteNTimes(0, (512 - (total_written % 512)) % 512);
+        self.ino += 1;
     }
 
-    pub fn deinit(self: *@This()) void {
-        self.entries.deinit();
+    pub fn addFile(self: *@This(), comptime path: []const u8, source: *std.io.StreamSource, perms: u32) !void {
+        try self.addEntry(source, path, .File, perms);
+    }
+
+    pub fn addDirectory(self: *@This(), comptime path: []const u8, perms: u32) !void {
+        try self.addEntry(null, path, .Directory, perms);
+    }
+
+    pub fn addSymlink(
+        self: *@This(),
+        comptime dstPath: []const u8,
+        comptime srcPath: []const u8,
+    ) !void {
+        var source = std.io.StreamSource{
+            .const_buffer = std.io.fixedBufferStream(srcPath),
+        };
+
+        try self.addEntry(
+            &source,
+            dstPath,
+            .Symlink,
+            0o777, // symlinks always have 777 perms
+        );
+    }
+
+    pub fn finalize(self: *@This()) !void {
+        try self.addEntry(null, TRAILER, .File, 0);
+
+        // Maintain a block size of 512 by adding padding to the end of the
+        // archive.
+        try self.dest.writer().writeByteNTimes(0, (512 - (self.total_written % 512)) % 512);
     }
 };
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-
     var args = std.process.args();
     _ = args.next().?;
     const init = args.next().?;
     const outfile = args.next().?;
 
-    var archive = try CpioArchive.init(arena.allocator());
-    defer archive.deinit();
+    var archive_file = try std.fs.cwd().createFile(outfile, .{});
+    defer archive_file.close();
+
+    var archive_file_source = std.io.StreamSource{ .file = archive_file };
+    var archive = try CpioArchive.init(&archive_file_source);
 
     var init_file = try std.fs.cwd().openFile(init, .{});
     defer init_file.close();
 
     var init_source = std.io.StreamSource{ .file = init_file };
-    try archive.addEntry(&init_source, "./init", .File, 0o755);
+    try archive.addFile("./init", &init_source, 0o755);
 
-    var archive_file = try std.fs.cwd().createFile(outfile, .{});
-    defer archive_file.close();
-
-    var archive_file_source = std.io.StreamSource{ .file = archive_file };
-    try archive.finalize(&archive_file_source);
+    try archive.finalize();
 }
