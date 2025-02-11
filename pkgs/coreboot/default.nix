d2f629115029f73fe0e7117588462f6a95d1d15c
Jared Baur <jaredbaur@fastmail.com>
2025-02-10 17:59:39 -0800

    Back to coreboot 24.08

--- pkgs/coreboot/default.nix
+++ pkgs/coreboot/default.nix
@@ -26,18 +26,16 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "coreboot-${board}";
-  version = "24.12";
+  version = "24.08";
   src =
     (fetchgit {
       url = "https://github.com/coreboot/coreboot";
-      rev = finalAttrs.version;
-      hash = "sha256-mdxYxE3JiHFDaftNVckeQTVOlF8sWccm74MrpgWtXb4=";
+      rev = "24.08";
+      hash = "sha256-l+TPBcAsbzYZgsLOPX30ZpgHINAByIIwRHzvjeirIvY=";
       fetchSubmodules = true;
     }).overrideAttrs
       (_: {
-        # Fetch the remaining submodules not fetched by the initial submodule
-        # fetch, since coreboot has `update = none` set on some submodules.
-        # See https://github.com/nixos/nixpkgs/blob/4c62505847d88f16df11eff3c81bf9a453a4979e/pkgs/build-support/fetchgit/nix-prefetch-git#L328
+        # https://github.com/nixos/nixpkgs/blob/4c62505847d88f16df11eff3c81bf9a453a4979e/pkgs/build-support/fetchgit/nix-prefetch-git#L328
         NIX_PREFETCH_GIT_CHECKOUT_HOOK = ''clean_git -C "$dir" submodule update --init --recursive --checkout -j ''${NIX_BUILD_CORES:-1} --progress'';
       });
   patches = [
