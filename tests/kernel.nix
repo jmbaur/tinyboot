e05d6c39fc0fc7f23395fb66af9887263c709516
Jared Baur <jaredbaur@fastmail.com>
2025-07-01 17:46:17 -0700

    Get console output on aarch64 tests

--- tests/kernel.nix
+++ tests/kernel.nix
@@ -23,6 +23,7 @@ linuxKernel.manualConfig {
       + ''
         CONFIG_64BIT=y
         CONFIG_FW_CFG_SYSFS=y
+        CONFIG_HVC_CONSOLE=y
         CONFIG_IKCONFIG=y
         CONFIG_PCI=y
         CONFIG_SCSI=y
@@ -32,20 +33,22 @@ linuxKernel.manualConfig {
         CONFIG_VIRTIO_BLK=y
         CONFIG_VIRTIO_CONSOLE=y
         CONFIG_VIRTIO_MENU=y
+        CONFIG_VIRTIO_MMIO=y
         CONFIG_VIRTIO_PCI=y
       ''
       + lib.optionalString stdenv.hostPlatform.isx86_64 ''
         CONFIG_ACPI=y
+        CONFIG_CMDLINE="debug console=ttyS0,115200"
         CONFIG_CMDLINE_BOOL=y
         CONFIG_CMDLINE_OVERRIDE=y
-        CONFIG_CMDLINE="debug console=ttyS0,115200"
         CONFIG_SERIAL_8250=y
         CONFIG_SERIAL_8250_CONSOLE=y
       ''
       + lib.optionalString stdenv.hostPlatform.isAarch64 ''
         CONFIG_ARM_SCMI_TRANSPORT_VIRTIO=y
-        CONFIG_CMDLINE_FORCE=y
         CONFIG_CMDLINE="debug console=ttyAMA0,115200"
+        CONFIG_CMDLINE_FORCE=y
+        CONFIG_PCI_HOST_GENERIC=y
         CONFIG_SERIAL_AMBA_PL011=y
         CONFIG_SERIAL_AMBA_PL011_CONSOLE=y
       '';
