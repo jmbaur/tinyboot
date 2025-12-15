a1ce4aeb6e9c293def2cae7214be3878ea28815a
Jared Baur <jaredbaur@fastmail.com>
2025-12-14 20:34:45 -0800

    Make dependency fetching one FOD

--- package.nix
+++ package.nix
@@ -1,92 +1,85 @@
 {
   firmwareDirectory ? null,
 
-  fetchzip,
   lib,
-  linkFarm,
   stdenvNoCC,
   zig_0_15,
 }:
 
-let
-  deps = linkFarm "tinyboot-deps" [
-    {
-      name = "clap-0.11.0-oBajB-3nAQAWHQx0oqlPOm4KLcvO4xEEvxg31WMlFh_Q";
-      path = fetchzip {
-        url = "https://github.com/jmbaur/zig-clap/archive/76f87a072e1be64834829ccba625aef0f0bd4fdd.tar.gz";
-        hash = "sha256-scA/YZ0ttRn6jaI2HliBQFyufP0T5mOdu1Ega1yTV2A=";
-      };
-    }
-    {
-      name = "N-V-__8AADE9jALWhPS2ykOLbf8gUmr9r-SBwyzzpccbtBAv";
-      path = fetchzip {
-        url = "https://github.com/MBED-TLS/mbedtls/archive/v3.6.4.tar.gz";
-        hash = "sha256-lSIdoqfIeBYJPQDBuKvXpPADZEqEuuFaJxM9LPhlgZ4=";
-      };
-    }
-    {
-      name = "N-V-__8AAPZ7fwBg4JoCzM_0o2A8wxH2hsUUeiU1iuZv53L5";
-      path = fetchzip {
-        url = "https://github.com/facebook/zstd/archive/v1.5.7.tar.gz";
-        hash = "sha256-tNFWIT9ydfozB8dWcmTMuZLCQmQudTFJIkSr0aG7S44=";
-      };
-    }
-  ];
-in
-stdenvNoCC.mkDerivation {
-  pname = "tinyboot";
-  version = "0.1.0";
+stdenvNoCC.mkDerivation (
+  finalAttrs:
+  let
+    deps = stdenvNoCC.mkDerivation {
+      pname = finalAttrs.pname + "-deps";
+      inherit (finalAttrs) src version;
+      depsBuildBuild = [ zig_0_15 ];
+      buildCommand = ''
+        export ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
+        runHook unpackPhase
+        cd $sourceRoot
+        zig build --fetch
+        mv $ZIG_GLOBAL_CACHE_DIR/p $out
+      '';
+      outputHashAlgo = null;
+      outputHashMode = "recursive";
+      outputHash = "sha256-GGlddHaG+3KzNmWvJuvbId7VTqLvTq/hvquZh86+alc=";
+    };
+  in
+  {
+    pname = "tinyboot";
+    version = "0.1.0";
 
-  src = lib.fileset.toSource {
-    root = ./.;
-    fileset = lib.fileset.unions [
-      ./build.zig
-      ./build.zig.zon
-      ./deps
-      ./src
+    src = lib.fileset.toSource {
+      root = ./.;
+      fileset = lib.fileset.unions [
+        ./build.zig
+        ./build.zig.zon
+        ./deps
+        ./src
+      ];
+    };
+
+    strictDeps = true;
+
+    depsBuildBuild = [ zig_0_15 ];
+
+    dontInstall = true;
+    doCheck = true;
+    dontStrip = true;
+
+    zigBuildFlags = [
+      "--color off"
+      "--release=safe"
+      "-Dtarget=${stdenvNoCC.hostPlatform.qemuArch}-${stdenvNoCC.hostPlatform.parsed.kernel.name}"
+    ]
+    ++ lib.optionals (firmwareDirectory != null) [
+      "-Dfirmware-directory=${firmwareDirectory}"
     ];
-  };
 
-  strictDeps = true;
+    configurePhase = ''
+      runHook preConfigure
 
-  depsBuildBuild = [ zig_0_15 ];
+      export ZIG_GLOBAL_CACHE_DIR=$TEMPDIR
 
-  dontInstall = true;
-  doCheck = true;
-  dontStrip = true;
+      ln -s ${deps} $ZIG_GLOBAL_CACHE_DIR/p
 
-  zigBuildFlags = [
-    "--color off"
-    "--release=safe"
-    "-Dtarget=${stdenvNoCC.hostPlatform.qemuArch}-${stdenvNoCC.hostPlatform.parsed.kernel.name}"
-  ]
-  ++ lib.optionals (firmwareDirectory != null) [
-    "-Dfirmware-directory=${firmwareDirectory}"
-  ];
+      runHook postConfigure
+    '';
 
-  configurePhase = ''
-    runHook preConfigure
+    buildPhase = ''
+      runHook preBuild
+      zig build install --prefix $out ''${zigBuildFlags[@]}
+      runHook postBuild
+    '';
 
-    export ZIG_GLOBAL_CACHE_DIR=$TEMPDIR
+    checkPhase = ''
+      runHook preCheck
+      zig build test ''${zigBuildFlags[@]}
+      runHook postCheck
+    '';
 
-    ln -s ${deps} $ZIG_GLOBAL_CACHE_DIR/p
+    passthru.initrdFile = "tboot-loader.cpio.zst";
 
-    runHook postConfigure
-  '';
-
-  buildPhase = ''
-    runHook preBuild
-    zig build install --prefix $out ''${zigBuildFlags[@]}
-    runHook postBuild
-  '';
-
-  checkPhase = ''
-    runHook preCheck
-    zig build test ''${zigBuildFlags[@]}
-    runHook postCheck
-  '';
-
-  passthru.initrdFile = "tboot-loader.cpio.zst";
-
-  meta.platforms = lib.platforms.linux;
-}
+    meta.platforms = lib.platforms.linux;
+  }
+)
